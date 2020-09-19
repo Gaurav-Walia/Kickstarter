ba4ff602a390421d16c9250c3c41009f3769faf7
Gaurav <gaurav.walia24@gmail.com>
2020-09-19 20:43:33 +0530

    removed log from modal.js

--- src/Modal.js
+++ src/Modal.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.css";
 class Modal extends React.Component {
 
     render() {
-        console.log(this.props);
         const { target, title, by, blurb, backers, pledged } = this.props;
         
         return(
